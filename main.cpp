7a18089e9a0e0ba2110d98eaeec1da618823b32c
haidar <haidaratmajaya0406@gmail.com>
2023-10-15 11:09:55 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "-------------------------------------------------------------------------------------------------" << endl;
+    cout << "Nama: Haidar Putra Atmajaya" << endl << endl;
+    cout << "NPM: 2310631170019" << endl << endl;
+    cout << "Cita-cita: Software Developer" << endl << endl;
+    cout << "Motivasi masuk Informatika: pastinya prospek kerja yangb sangat luas" << endl << endl;
+    cout << "dan ingin mengikuti perkembangan teknologi" << endl;
+    cout << "------------------------------------------------------------------------------------------------" << endl;
+
+
+
+
+
+     return 0;
+
+
+
+}
+
